68cb06b514e9350fb9d714722cdb29d2e7b8d738
willyb321 <wbwilliam7@gmail.com>
2017-07-06 09:25:32 +1000

    only rate limit in prod, .idea, lint

--- index.js
+++ index.js
@@ -1,22 +1,26 @@
 const zlib = require('zlib');
 const zmq = require('zeromq');
-const sock = zmq.socket('sub');
 const express = require('express');
-const MongoClient = require('mongodb').MongoClient
+const MongoClient = require('mongodb').MongoClient;
 const Raven = require('raven');
 const moment = require('moment');
-const paginate = require("paginate-array");
+const paginate = require('paginate-array');
 const RateLimit = require('express-rate-limit');
+const isDev = require('is-dev');
+
+const sock = zmq.socket('sub');
 
 const app = express();
 
 const apiLimiter = new RateLimit({
-  windowMs: 15*60*1000,
-  max: 75,
-  delayMs: 0,
-  headers: true
+	windowMs: 15 * 60 * 1000,
+	max: 75,
+	delayMs: 0,
+	headers: true
 });
-app.use('/api/', apiLimiter);
+if (!isDev) {
+	app.use('/api/', apiLimiter);
+}
 Raven.config('https://7c3174b16e384349bbf294978a65fb0c:c61b0700a2894a03a46343a02cf8b724@sentry.io/187248').install();
 
 const url = 'mongodb://localhost:54373/eddn';
@@ -39,20 +43,20 @@ sock.on('message', topic => {
 					message.message.unixTimestamp = moment(message.message.timestamp).valueOf();
 					message.message.software = `${message.header.softwareName}@${message.header.softwareVersion}`;
 					const collection = db.collection('eddnHistory');
-					collection.insertOne(message.message).then(result => {
+					collection.insertOne(message.message).then(() => {
 						console.log('inserted ' + message.message.event + ' from: ' + message.message.uploader);
 						db.close();
 					}).catch(err => {
 						console.error(err);
 						Raven.captureException(err);
 						db.close();
-					})
+					});
 				}).catch(err => {
 					Raven.captureException(err);
 					console.error(err);
 				});
 		}
-	})
+	});
 });
 
 function connectDB() {
@@ -66,24 +70,24 @@ function connectDB() {
 		});
 	});
 }
-app.get('/', (req, res, next) => {
+app.get('/', (req, res) => {
 	res.send('hello');
 });
 
-app.get('/api/cmdr/:cmdr', (req, res, next) => {
+app.get('/api/cmdr/:cmdr', (req, res) => {
 	const cmdr = req.params.cmdr.toLowerCase();
 	const page = req.query.page;
 	if (!page) {
-		console.log('No page query, sending first 50');
+		console.log('No page query, sending first 25');
 	}
 	connectDB()
 		.then(db => {
 			const collection = db.collection('eddnHistory');
-			collection.find({uploader: cmdr}).sort({ unixTimestamp: -1 }).toArray((err, docs) => {
+			collection.find({uploader: cmdr}).sort({unixTimestamp: -1}).toArray((err, docs) => {
 				if (err) {
 					console.error(err);
 					Raven.captureException(err);
-				};
+				}
 				const paginated = paginate(docs, page || 1, 25);
 				res.json(paginated);
 				db.close();
@@ -91,24 +95,23 @@ app.get('/api/cmdr/:cmdr', (req, res, next) => {
 		}).catch(err => {
 			Raven.captureException(err);
 			console.error(err);
-			db.close();
-		})
+		});
 });
 
-app.get('/api/system/:system', (req, res, next) => {
+app.get('/api/system/:system', (req, res) => {
 	const system = req.params.system;
 	const page = req.query.page;
 	if (!page) {
-		console.log('No page query, sending first 50');
+		console.log('No page query, sending first 25');
 	}
 	connectDB()
 		.then(db => {
 			const collection = db.collection('eddnHistory');
-			collection.find({StarSystem: system}).sort({ unixTimestamp: -1 }).toArray((err, docs) => {
+			collection.find({StarSystem: system}).sort({unixTimestamp: -1}).toArray((err, docs) => {
 				if (err) {
 					console.error(err);
 					Raven.captureException(err);
-				};
+				}
 				const paginated = paginate(docs, page || 1, 25);
 				res.json(paginated);
 				db.close();
@@ -116,11 +119,10 @@ app.get('/api/system/:system', (req, res, next) => {
 		}).catch(err => {
 			Raven.captureException(err);
 			console.error(err);
-			db.close();
-		})
+		});
 });
 
-app.get('/api/station/:station', (req, res, next) => {
+app.get('/api/station/:station', (req, res) => {
 	const station = req.params.cmdr;
 	const page = req.query.page;
 	if (!page) {
@@ -129,11 +131,11 @@ app.get('/api/station/:station', (req, res, next) => {
 	connectDB()
 		.then(db => {
 			const collection = db.collection('eddnHistory');
-			collection.find({StationName: station}).sort({ unixTimestamp: -1 }).toArray((err, docs) => {
+			collection.find({StationName: station}).sort({unixTimestamp: -1}).toArray((err, docs) => {
 				if (err) {
 					console.error(err);
 					Raven.captureException(err);
-				};
+				}
 				const paginated = paginate(docs, page || 1, 25);
 				res.json(paginated);
 				db.close();
@@ -141,9 +143,8 @@ app.get('/api/station/:station', (req, res, next) => {
 		}).catch(err => {
 			Raven.captureException(err);
 			console.error(err);
-			db.close();
-		})
+		});
 });
-app.listen(5125, function () {
-	console.log('Server listening on 5125')
-})
+app.listen(5125, () => {
+	console.log('Server listening on 5125');
+});
